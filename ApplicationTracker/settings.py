cd8fe63201fe912d4682f8d2657debad9d9346b3
DsrKrasniii <donredd3rd@gmail.com>
2022-12-13 13:06:36 -0700

    round 2

--- ApplicationTracker/settings.py
+++ ApplicationTracker/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,14 +80,10 @@ WSGI_APPLICATION = 'ApplicationTracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'applicationtracker',
-        'USER': 'postgres',
-        'PASSWORD': 'brobrobeanz',
-        'HOST': 'localhost'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
